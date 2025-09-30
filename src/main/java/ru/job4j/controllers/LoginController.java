ec3ceab0b0ff2d82bd3da2ada5883429d88395d3
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-09-30 11:49:48 +0300

    0. Spring Security [#6879]

---
+++ src/main/java/ru/job4j/controllers/LoginController.java
@@ -0,0 +1,38 @@
+package ru.job4j.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        @RequestParam(value = "logout", required = false) String logout,
+                        Model model) {
+        String errorMessage = null;
+        if (error != null) {
+            errorMessage = "Username or password is incorrect !!";
+        }
+        if (logout != null) {
+            errorMessage = "You have been seccessfully logged out !!";
+        }
+        model.addAttribute("errorMessage", errorMessage);
+        return "fragments/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(httpServletRequest, httpServletResponse, authentication);
+        }
+        return "redirect:/login?logout=true";
+    }
+}
